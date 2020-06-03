9df795a50471fea41794cb573a716ba3aa3c5161
Reinos <info@reinos.nl>
2020-06-03 16:16:52 +0200

    update

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  setInterval(() => console.log(`some random number ${Math.random()}`), 1000);
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST, () => {
+  setInterval(() => console.log(`some random number ${Math.random()}`), 1000);
+  console.log(`Running on http://${HOST}:${PORT}`)
+});
