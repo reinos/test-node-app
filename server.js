6f81771728d6ce7377bd665f1355ebb10334c5c6
Reinos <info@reinos.nl>
2020-06-03 15:35:45 +0200

    update response

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv"); 
+const dotenv = require("dotenv");
 dotenv.config();
 
 // if you want to run locally change port to 3000
 // for Apphosting use port 80
-const PORT = 80; 
+const PORT = 8009;
 
 // to deploy it to AppHosting please use IP:0.0.0.0
 const HOST = '0.0.0.0';
@@ -12,7 +12,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  setInterval(() => console.log(`some random md5 number ${Math.random()}`), 1000);
+  setInterval(() => console.log(`some random number ${Math.random()}`), 1000);
+  res.send('Hello World!');
 });
 
 app.listen(PORT, HOST);
