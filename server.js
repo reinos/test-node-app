b28eba6f8b5aeca0056493754718ff6b9d8b1a79
Rein <info@reinos.nl>
2020-06-03 13:58:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv"); 
 dotenv.config();
 
-const jexiaSDK = require("jexia-sdk-js/node");
-const dataModule = jexiaSDK.dataOperations();
-const field = jexiaSDK.field();
-
-const credentials = {
-  projectID: process.env.PROJECT_ID,
-  key: process.env.API_KEY,
-  secret:process.env.API_SECRET
-};
-
-jexiaSDK.jexiaClient().init( credentials,dataModule);
-const todo = dataModule.dataset(process.env.DATASET);
-
 // if you want to run locally change port to 3000
 // for Apphosting use port 80
 const PORT = 80; 
@@ -25,21 +12,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  todo
-  .select()
-  .limit(200)
-  .fields("text", "id","done")
-  .where(field=> field("text").isLike("%4").and(field("id").isNotNull()))
-  .subscribe(
-   records => { 
-    console.log(records)
-    res.send(records)
-  }, 
-  error => { 
-     console.log(error)
-     next(error)
-  }
- );  
+  setInterval(() => console.log(`some random md5 number ${Math.random()}`), 1000);
 });
 
 app.listen(PORT, HOST);
