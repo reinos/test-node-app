ea90dd503c58d7333a2a3462fdff4aff86f464b2
Reinos <info@reinos.nl>
2020-06-03 19:44:16 +0200

    update

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  setInterval(() => console.log(`some random number ${Math.random()}`), 10000);
+  setInterval(() => console.log(`some random number and updated ${Math.random()}`), 10000);
 });
